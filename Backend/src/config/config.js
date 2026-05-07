c6bc871927e1b323b470927bc2451f9041692e4b
itsMilanCodesu <milantamang350359@gmail.com>
2026-05-08 08:01:28 +0900

    feat:passport config added

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -13,7 +13,10 @@ const config = {
 
     MONGO_URI : String(process.env.MONGO_URI),
     PORT : String(process.env.PORT),
-    JWT_SECRET : String(process.env.JWT_SECRET)
+    JWT_SECRET : String(process.env.JWT_SECRET), 
+    GOOGle_CLIENT_ID : String(process.env.Google_CLIENT_ID),
+    GOOGLE_CLIENT_SECRET : String(process.env.GOOGLE_CLIENT_SECRET),
+    GOOGLE_CALLBACK_URL : String(process.env.GOOGLE_CALLBACK_URL)
 
 }
 
