2fce8b9f749089be569538e74a9fd7d86a0a15cf
itsMilanCodesu <milantamang350359@gmail.com>
2026-05-07 13:20:56 +0900

    feat : login api created

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import {Router} from "express";
-import { validateRegisterUser } from "../validators/auth.validator.js";
-import { registerController } from "../controllers/auth.controller.js";
+import { validateLoginUser, validateRegisterUser } from "../validators/auth.validator.js";
+import { LoginController, registerController } from "../controllers/auth.controller.js";
 
 
 const authRouter = Router();
 
 
 authRouter.post("/register",validateRegisterUser, registerController )
-
+authRouter.post("/login",validateLoginUser ,LoginController)
 
 
 
